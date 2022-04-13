e5dc1ac419c2c0f9323f6ef1f695921c57ace2dd
mvanzo <97002794+mvanzo@users.noreply.github.com>
2022-04-13 15:02:09 -0700

    useEffect set up for product db on page load, started mapping through array

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function Shop(){
+    const [products, setProducts] = useState([])
+
+    useEffect(()=> {
+        const fetchData = async ()=> {
+            const result = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/products`)
+            console.log(result.data.findAllProducts)
+            setProducts(result.data.findAllProducts)
+        }
+        fetchData()
+    }, [])
+
+    const mappedProducts = products.map((product, idx)=> {
+        return(
+            <p key={idx}>{product.name}</p>
+        )
+    })
+
     return(
+        <>
         <h1>Shopping Page</h1>
+        <h3>Featured Products</h3>
+        {mappedProducts}
+        </>
     )
 }
