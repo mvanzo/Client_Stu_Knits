e89afd6eca259d43c5f4f15f1a4432f6e38058c9
mvanzo <97002794+mvanzo@users.noreply.github.com>
2022-04-17 15:03:39 -0700

    fixed orders page bug

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 
 export default function Orders({ currentUser }) {
     const [orders, setOrders] = useState([])
+    const [user, setUser] = useState('')
     
     useEffect( ()=>{
         findOrders()
@@ -10,20 +11,20 @@ export default function Orders({ currentUser }) {
 
     const findOrders = async () => {
         let response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/findorders`)
-        console.log(response.data)
         setOrders(response.data)
     }
 
     if (currentUser && currentUser.admin == true) {
         console.log('admin is logged in ')
-    } else if (currentUser && currentUser.admin == true) {
+        // setUser('admin')
+    } else if (currentUser && currentUser.admin == false) {
         console.log('non admin is logged in')
+        // setUser('user')
     } else {
         console.log('not logged in')
     }
 
     // admin mapping of orders data
-
     const adminOrders = orders.map((order, idx)=> {
         return(
             <div key={idx}>
@@ -39,15 +40,17 @@ export default function Orders({ currentUser }) {
         )
     })
 
-    if (currentUser) {
+    let userOrders = null
+
+    if (currentUser && currentUser.admin == false) {
         const filteredOrders = orders.filter(item=>{
-            console.log(item.billing_details.email)
+            // console.log(item.billing_details.email)
             if (item.billing_details.email == currentUser.email) {
                 return true
             }
         })
 
-        const userOrders = filteredOrders.map((order, idx)=> {
+        userOrders = filteredOrders.map((order, idx)=> {
             return(
                 <div key={idx}>
                     <br />
@@ -63,32 +66,11 @@ export default function Orders({ currentUser }) {
         })
     }
 
-    // const userOrders = filteredOrders.map((order, idx)=> {
-    //     return(
-    //         <div key={idx}>
-    //             <br />
-    //             {/* <p>{order.customer_details}</p> */}
-    //             <p>Order Total: ${order.amount/100}</p>
-    //             <p>Name: {order.billing_details.name}</p>
-    //             <p>email: {order.billing_details.email}</p>
-    //             <p>{order.customer}</p>
-    //             <p>Order ID: {order.id}</p>
-    //             <br />
-    //         </div>
-    //     )
-    // })
-
-    // console.log(userOrders)
-
     return(
         <div>
             <h1>this is the orders page</h1>
-            {currentUser && currentUser.admin == true ? adminOrders : 'no orders to review'}
-            {/* {filteredOrders} */}
-            {currentUser ? userOrders : '' }
-
-            {currentUser }
-
+            {currentUser && currentUser.admin == true ? adminOrders : ''}
+            {userOrders}
         </div>
     )
 }
